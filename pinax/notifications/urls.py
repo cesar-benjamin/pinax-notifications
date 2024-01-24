5a46ee8c027b92d1b0fc630d87d551cb3cd2c943
César Benjamín <mathereall@gmail.com>
2024-01-23 20:43:42 -0600

    Update urls.py
    the shape of path match with old url shape, but for that must be used re_path instead of path

--- pinax/notifications/urls.py
+++ pinax/notifications/urls.py
@@ -5,5 +5,5 @@ from .views import NoticeSettingsView
 app_name = "pinax_notifications"
 
 urlpatterns = [
-    path(r"^settings/$", NoticeSettingsView.as_view(), name="notice_settings"),
+    re_path(r"^settings/$", NoticeSettingsView.as_view(), name="notice_settings"),
 ]
